c984e22a15c9a2953ed2aa6268ba75094db86401
SandeepAgrahari <sagrahari399@gmail.com>
2025-09-22 19:31:39 +0530

    fix: bg color of profile list is changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,21 +43,21 @@ const Navbar = () => {
             </div>
             <ul
               tabIndex={0}
-              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+              className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
-              <li>
+              <li className="hover:bg-pink-400">
                 <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
                 </Link>
               </li>
-              <li>
+              <li className="hover:bg-pink-400">
                 <Link to="/connections">Connection</Link>
               </li>
-              <li>
+              <li className="hover:bg-pink-400">
                 <Link to="/requests">Request</Link>
               </li>
-              <li>
+              <li className="hover:bg-pink-400">
                 <a onClick={handleLogout}>Logout</a>
               </li>
             </ul>
